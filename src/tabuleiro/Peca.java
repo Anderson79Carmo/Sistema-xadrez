557dc8b77ba3291e86d4e81fcf28cc7ff4711553
Anderson79Carmo <anderson_csilva@yahoo.com.br>
2021-04-26 19:40:17 -0300

    Iniciando Tabuleiro e Peca

---
+++ src/tabuleiro/Peca.java
@@ -0,0 +1,23 @@
+package tabuleiro;
+
+public class Peca {
+	
+	// protect para não ficar visivel em outro pacote
+	
+	protected Posicao posicao;
+	private Tabuleiro tabuleiro;
+	
+	public Peca(Tabuleiro tabuleiro) {
+		this.tabuleiro = tabuleiro;
+		posicao = null;
+	}
+
+	protected Tabuleiro getTabuleiro() {
+		return tabuleiro;
+	}
+
+
+	
+	
+
+}
